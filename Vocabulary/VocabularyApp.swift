96adb299c3423ed75b232023f7abe8e02e4031a5
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-08-18 17:12:16 +0200

    Initial Commit

---
+++ Vocabulary/VocabularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyApp.swift
+//  Vocabulary
+//
+//  Created by Leonard Fekete on 18.08.24.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
